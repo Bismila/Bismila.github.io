2f5244938c3aa5ddefa6cf3f0b6203d3331117c4
Olesya Shpikovskaya <bismiladance@gmail.com>
2017-06-25 18:33:49 +0300

    Arena - landing page

---
+++ arena/js/js.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function() {
+   //Initialize our slider
+        $('.sl_1').slick({
+            arrows: false,
+            autoplay:true,
+            autoplaySpeed:3000,
+            infinite: true,
+            dots:true,
+            // dotsClass:'dots'
+            // responsiveBaseWidth:'1920'
+        });
+        $('.sl_2').slick({
+            arrows: false,
+            autoplay:true,
+            autoplaySpeed:3000,
+            infinite: true,
+            dots:true,
+            // dotsClass:'slick-dots'
+
+        });
+
+//scroll down
+    $(".yakor").on("click", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href');
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+        var top = $(id).offset().top;
+
+        console.log(top);
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+
+
+
+});
