06c7c5f4864c615be48d51e555a6ddf5f3bf7185
Olesya Shpikovskaya <bismiladance@gmail.com>
2017-06-25 18:30:23 +0300

    Nail Brand - test project
    Одна из страниц сайта - задание тестовое

---
+++ NailBrand/js/js.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function(){
+    $('.up').on('click', function(e){
+        $('html,body').stop().animate({ scrollTop: $('#up').offset().top }, 800);
+        e.preventDefault();
+    });
+
+    $("#drop_sidebar").click(function () {
+        $(".left_menu_ul").slideToggle("slow");
+    })
+
+    });
